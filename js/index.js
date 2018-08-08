d1e07d7982599682470d65db6b821407d28769e5
Andrew <fedakandrew@gmail.com>
2018-08-08 13:16:16 +0000

    Initial

--- js/index.js
+++ js/index.js
@@ -1,13 +1,18 @@
 const display = document.querySelector('.calc .display');
 
 document.querySelectorAll('.calc .num, .calc .plus, .calc .minus, .calc .multiply, .calc .divide')
-    .forEach( button => button.addEventListener('click', numOperClick));
+    .forEach(button => button.addEventListener('click', numOperClick));
 
 function numOperClick(e) {
     display.value += e.target.innerText;
 }
 
 document.querySelector('.calc .eq')
-    .addEventListener('click', function(){
-        display.value = eval(display.value);        
-    })
+    .addEventListener('click', function() {
+        display.value = eval(display.value);
+    });
+
+document.querySelector('.calc .clear')
+    .addEventListener('click', function () {
+        display.value = ' ';
+});
